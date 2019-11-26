3c19e2b1adc3a7a933d67a48f4690486bd17e5ff
mohabashraf <mohabashraf466@gmail.com>
2019-11-26 13:49:54 +0200

    Add LDAP authentication

--- src/main/java/com/ocp/ldap/SecurityConfiguration.java
+++ src/main/java/com/ocp/ldap/SecurityConfiguration.java
@@ -1,9 +1,25 @@
 package com.ocp.ldap;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new LdapShaPasswordEncoder())
+                .passwordAttribute("userPassword");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
