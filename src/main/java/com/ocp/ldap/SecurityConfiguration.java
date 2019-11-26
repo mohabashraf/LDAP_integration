fef3858c29efbfca6f5fa6de0d1d2dd6e6413d7c
mohabashraf <mohabashraf466@gmail.com>
2019-11-26 13:46:36 +0200

    Override authorization

--- src/main/java/com/ocp/ldap/SecurityConfiguration.java
+++ src/main/java/com/ocp/ldap/SecurityConfiguration.java
@@ -1,7 +1,17 @@
 package com.ocp.ldap;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin();
+    }
 
 }
